e43112f1687c9d3062cc22b4e6be602878777f61
daianamuf <muflicdaiana@gmail.com>
2024-02-28 16:32:11 +0200

    add pagination for blog

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -6,6 +6,8 @@ import Loader from "../components/Loader";
 function Blog() {
   const [posts, setPosts] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage] = useState(10);
 
   useEffect(() => {
     const query = encodeURIComponent(`*[_type == "post"]{
@@ -48,13 +50,22 @@ function Blog() {
     return `${day}/${month}/${year}`;
   };
 
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+  const totalPages = Math.ceil(posts.length / postsPerPage);
+
   if (isLoading) return <Loader />;
 
   return (
     <div className="blog">
       <h1 className="blog__heading">Blog</h1>
+      <p className="blog__page">Pagina {currentPage}</p>
       <section className="blog__posts">
-        {posts.map((post) => (
+        {currentPosts.map((post) => (
           <div key={post._id} className="blog__post">
             <Link to={`/blog/${post.slug}`} className="blog__post--btn">
               <img
@@ -71,6 +82,23 @@ function Blog() {
           </div>
         ))}
       </section>
+
+      <div className="blog__pagination">
+        {Array.from({ length: totalPages }, (_, index) => (
+          <button
+            // className=""
+            className={
+              currentPage === index + 1
+                ? "blog__pagination--btn blog__pagination--active"
+                : "blog__pagination--btn"
+            }
+            key={index + 1}
+            onClick={() => paginate(index + 1)}
+          >
+            {index + 1}
+          </button>
+        ))}
+      </div>
     </div>
   );
 }
